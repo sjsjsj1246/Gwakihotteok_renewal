8793ef233d3c1a77aa3c25b408eedd960f61ff58
Hwang In Seo <sjsjsj1246@gmail.com>
2020-12-14 23:55:08 +0900

    first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,9 @@
 import './app.css';
+import Habit from './components/habit';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Habit></Habit>
   );
 }
 
---
+++ src/components/habit.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = (event) => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = (event) => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
+  render() {
+    return (
+      <li className="habit">
+        <span className="habit-name">Reading</span>
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button className="habit-button habit-delete">
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
